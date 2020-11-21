8d2aae6dcc6a930132604c748e2c6c5e9c7f2d3a
Yilin Ma <1135485226@qq.com>
2020-11-20 19:32:09 -0800

    After test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const eventHandler = async () => {
+    const events = new EventSource('/events');
+    events.onmessage = async (event) => {
+      const resp = await fetch('/api');
+      const respJson = await resp.json();
+      setTasks(respJson.tasks);
+    };
+  };
+
+  useEffect(() => {
+    eventHandler();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Streaming API</h1>
+      <h1>HTTP SSE</h1>
+      <ul>
+        {tasks.map((task, idx) => (
+          <li key={idx.toString()}>{task.task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
